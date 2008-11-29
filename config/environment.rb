a8506f27c66fcc4bc998b52c7a151f1a7e485441
Matt Hawkins <matthew.b.hawkins@gmail.com>
2008-11-29 00:40:45 -0500

    correctly added quietbacktrace

--- config/environment.rb
+++ config/environment.rb
@@ -11,6 +11,7 @@ RAILS_GEM_VERSION = '2.2.2' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
+  config.gem "thoughtbot-quietbacktrace", :lib => "quietbacktrace", :source => "http://gems.github.com"
   config.time_zone = 'UTC'
 
   config.action_controller.session = {

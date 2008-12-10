0bf04ad4c3980f7787840a40368dec9a39a2f936
Matt Hawkins <matthew.b.hawkins@gmail.com>
2008-12-09 21:28:57 -0500

    started flushing out functionality of movie admin interface

--- app/controllers/admin/movies_controller.rb
+++ app/controllers/admin/movies_controller.rb
@@ -1,11 +1,47 @@
 class Admin::MoviesController < ApplicationController
   def index
+    @movies = Movie.find(:all)
   end
 
   def new
+    @movie = Movie.new
+  end
+  
+  def create
+   @movie = Movie.new(params[:movie])
+
+    if @movie.save
+      flash[:notice] = "Created movie '#{@movie.title}'."
+      redirect_to admin_movies_path
+    else
+      flash[:error] = "Failed to create movie."
+      render :action => :new
+    end
   end
 
   def edit
+   @movie = Movie.find(params[:id])
+  end
+  
+  def update
+    @movie = Movie.find(params[:id])
+    
+    if @movie.update_attributes(params[:movie])
+      flash[:notice] = "Update movie '#{@movie.title}'."
+      redirect_to admin_movies_path
+    else
+      flash[:error] = "Failed to update movie '#{@movie.title}'."
+      render :action => :edit
+    end
   end
 
+
+  def destroy
+    @movie = Movie.find(params[:id])
+    
+    flash[:notice] = "Removed movie '#{@movie.title}'."
+    @movie.destroy
+    redirect_to admin_movies_path
+  end
+  
 end
--- test/functional/admin/movies_controller_test.rb
+++ test/functional/admin/movies_controller_test.rb
@@ -1,8 +1,88 @@
 require 'test_helper'
 
 class Admin::MoviesControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  
+  context "An Admin::MoviesController" do
+    
+    context "not logged in" do
+      
+      context "on GET to index" do
+        setup do
+          get :index
+        end
+        
+        should_redirect_to "new_session_path"
+      end
+      
+      context "on GET to new" do
+        setup do
+          get :new
+        end
+        
+        should_redirect_to "new_session_path"
+      end
+      
+      context "on POST to create" do
+        setup do
+          post :create
+        end
+        
+        should_redirect_to "new_session_path"
+      end
+      
+      context "on GET to edit" do
+        setup do
+          get :edit
+        end
+        
+        should_redirect_to "new_session_path"
+      end
+      
+      context "on PUT to update" do
+        setup do
+          put :update
+        end
+        
+        should_redirect_to "new_session_path"
+      end
+      
+      context "on DELETE to destroy" do
+        setup do
+          delete :destroy
+        end
+        
+        should_redirect_to "new_session_path"
+      end
+      
+    end
+    
+    context "logged in" do
+      
+      setup do
+        login_as :quentin
+      end
+      
+      context "on GET to index" do
+        setup do
+          get :index
+        end
+        
+        should_render_template :index
+        should_assign_to :movies
+      end
+      
+      context "on GET to new" do
+        setup do
+          get :new
+        end
+        
+        should_render_template :new
+        should_assign_to :movie
+        should_render_a_form
+      end
+      
+    end
+    
   end
+  
 end
